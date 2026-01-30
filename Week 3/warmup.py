c42cd8843217f02e3b51606bfa59b334d7fc177c
skellypro <s.kellypro511@gmail.com>
2026-01-29 17:30:23 -0700

    This week's Contributions

---
+++ Week 3/warmup.py
@@ -0,0 +1,14 @@
+due_tomorrow = False
+motivated = True ## fixed naming
+understand_material = True
+have_time = True
+tired = True
+
+mentally_ready = motivated and understand_material and not tired
+good_timing = have_time or due_tomorrow
+
+start_assignment = mentally_ready or good_timing
+
+print("mentally_ready:", mentally_ready)
+print("good_timing:", good_timing)
+print("start_assignment:", start_assignment)
