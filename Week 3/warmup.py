993daa6375a03c270e61fad1378c4a33d66791d7
skellypro <s.kellypro511@gmail.com>
2026-02-07 01:01:34 -0700

    Working on Programming assignment 2

--- Week 3/warmup.py
+++ Week 3/warmup.py
@@ -12,3 +12,11 @@ start_assignment = mentally_ready or good_timing
 print("mentally_ready:", mentally_ready)
 print("good_timing:", good_timing)
 print("start_assignment:", start_assignment)
+
+number = input("enter a number greater than 0: ")
+intNumber = int(number)
+
+if intNumber > 0:
+    total = 100 / intNumber
+else:
+    print("user ID:10T Error") # that was funny
