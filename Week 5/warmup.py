86cfd6e9080c5828377d59a4803ba367e500a07c
skellypro <s.kellypro511@gmail.com>
2026-02-12 13:55:13 -0700

    Week 5 warmup... taking it too far

--- Week 5/warmup.py
+++ Week 5/warmup.py
@@ -1,3 +1,19 @@
+import sys
+import time
+
+MAX_ATTEMPTS = 3
+CORRECT = "swordfish"  # this is the correct password
+attempts = 0
+password = ""
+while (CORRECT != password) and (MAX_ATTEMPTS > attempts):
+    time.sleep(attempts * attempts)
+    password = input("Password to launch: ")
+    attempts += 1
+else:
+    if MAX_ATTEMPTS == attempts:
+        # raise SystemExit
+        sys.exit("Lockdown initiated. Security has been notified. May God have mercy on your soul...")
+
 i = 3
 while i:
     print(str(i) + "...")
