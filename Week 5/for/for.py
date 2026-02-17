4508105ec5955c5a32b4324fd265cb99e394bffc
skellypro <s.kellypro511@gmail.com>
2026-02-17 13:55:38 -0700

    Week 6 participation

--- Week 5/for/for.py
+++ Week 5/for/for.py
@@ -1,6 +1,11 @@
-for j in range(5):
-    password = input("Enter password: ")
+import time
+
+for j in range(3):
+    time.sleep(j * j)
+    password = input("Password attempt " + str(j + 1) + ": ")
     if "swordfish" == password:
         for k in range(3, 0, -1):
             print(k)
+            time.sleep(1)
+        print("Ignition")
         break
