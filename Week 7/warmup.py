179a3c89ce1d7e192e456f861685cd526eefaf71
skellypro <s.kellypro511@gmail.com>
2026-02-24 13:03:59 -0700

    Week 7 Tuesday Warmup

--- Week 7/warmup.py
+++ Week 7/warmup.py
@@ -5,7 +5,6 @@ servers = ["Server1", "192.168.2.10", "db-server"]
 for server in servers:
 # and loops through a range of common ports (80-84)
     for port in range(80, 85):
-        print(f"Scanning {server} on port {port}")
-
-#output to look like the following:
-# Scanning {server} on port x
+        #output to look like the following:
+        # Scanning {server} on port x
+        print("Scanning", server, "on port", port)
